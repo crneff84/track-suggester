a3b52c5d32c7ecc48a97018797dc6e458dba8fd2
Chance Neff <crneff84@gmail.com>
2016-08-12 12:04:11 -0700

    update javascript with logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,23 @@ $(document).ready(function(){
     var skillInput = $("input:radio[name=skill]:checked").val();
     var macorpcInput = $("input:radio[name=macorpc]:checked").val();
 
-    alert(macorpcInput)
+    if (creativeInput === 'No' && companyInput === 'newrelic' && skillInput === 'web' || skillInput === 'web' && startupInput === 'yes'){
+      $('#ruby').show();
+    } else if (creativeInput === 'No' && companyInput === 'intel' || startupInput === 'no' && skillInput === 'data'){
+      $('#php').show();
+    } else if (creativeInput === 'No' && startupInput === 'yes' && skillInput === 'mobile' || companyInput === 'nike' && macorpcInput === 'both' && skillInput === 'mobile'){
+      $('#java').show();
 
 
+    } else if (creativeInput === 'Yes' && startupInput === 'yes' && skillInput === 'web' || creativeInput === 'Yes' && startupInput === 'no'){
+      $('#css').show();
+    } else if (creativeInput === 'No' && startupInput === 'yes' && skillInput === 'web'){
+      $('#c').show();
+    } else {
+      $('#alternate').show();
+    }
 
-
+    $('#outro').show();
     event.preventDefault();
   });
 });
