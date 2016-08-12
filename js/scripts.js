c696a68b9dd7c17d7be0454ce6ada11e5cba41bd
Chance Neff <crneff84@gmail.com>
2016-08-12 14:27:52 -0700

    fix issue for when name is not entered by user

--- js/scripts.js
+++ js/scripts.js
@@ -7,46 +7,58 @@ $(document).ready(function(){
     var skillInput = $("input:radio[name=skill]:checked").val();
     var macorpcInput = $("input:radio[name=macorpc]:checked").val();
 
-    if (creativeInput === 'No' && companyInput === 'newrelic' && skillInput === 'web' || skillInput === 'web' && startupInput === 'yes'){
+    if (nameInput === ""){
+      alert("Please Enter Your Name")
+    } else if (creativeInput === 'No' && companyInput === 'newrelic' && skillInput === 'web' || skillInput === 'web' && startupInput === 'yes'){
       $('#ruby').show();
       $('#php').hide();
       $('#java').hide();
       $('#css').hide();
       $('#c').hide();
+      $(".user").text(nameInput);
+      $('#outro').show();
     } else if (creativeInput === 'No' && companyInput === 'intel' || startupInput === 'no' && skillInput === 'data'){
       $('#php').show();
       $('#ruby').hide();
       $('#java').hide();
       $('#css').hide();
       $('#c').hide();
+      $(".user").text(nameInput);
+      $('#outro').show();
     } else if (creativeInput === 'No' && startupInput === 'yes' && skillInput === 'mobile' || companyInput === 'nike' && macorpcInput === 'both' && skillInput === 'mobile'){
       $('#java').show();
       $('#php').hide();
       $('#ruby').hide();
       $('#css').hide();
       $('#c').hide();
+      $(".user").text(nameInput);
+      $('#outro').show();
     } else if (creativeInput === 'Yes' && startupInput === 'yes' && skillInput === 'web' || creativeInput === 'Yes' && startupInput === 'no' && skillInput === 'web'){
       $('#css').show();
       $('#php').hide();
       $('#java').hide();
       $('#ruby').hide();
       $('#c').hide();
+      $(".user").text(nameInput);
+      $('#outro').show();
     } else if (creativeInput === 'No' && startupInput === 'no' && skillInput === 'game' || skillInput === 'game' && macorpcInput === 'pc' || companyInput === 'microsoft' && macorpcInput === 'pc'){
       $('#c').show();
       $('#php').hide();
       $('#java').hide();
       $('#css').hide();
       $('#ruby').hide();
+      $(".user").text(nameInput);
+      $('#outro').show();
     } else {
       $('#java').show();
       $('#php').hide();
       $('#ruby').hide();
       $('#css').hide();
       $('#c').hide();
+      $(".user").text(nameInput);
+      $('#outro').show();
     }
 
-    $(".user").text(nameInput);
-    $('#outro').show();
     event.preventDefault();
   });
 });
