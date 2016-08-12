0b75fd2b722700569275234aa8cb6f271ba3e211
Chance Neff <crneff84@gmail.com>
2016-08-12 10:59:13 -0700

    define js variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   $("form#suggester").submit(function(event){
+    var nameInput = $("#person").val();
+    var creativeInput = $("#creative").val();
+    var companyInput = $("input:radio[name=company]:checked").val();
+    var startupInput = $("input:radio[name=startup]:checked").val();
+    var skillInput = $("input:radio[name=skill]:checked").val();
+    var macorpcInput = $("input:radio[name=macorpc]:checked").val();
 
+    alert(macorpcInput)
 
 
 
