7e2ee648096fb92a647bffbf229875594dda10b1
Chance Neff <chance.r.neff@gmail.com>
2019-07-16 15:55:01 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -75,5 +75,5 @@ var startTime = Date.now();
 $(window).on("load", function(){
   setTimeout(function(){
     var totalTimeElapsed = Date.now() - startTime;
-    alert("Alerted at: " + totalTimeElapsed); }, 500);
+    alert("Alerted at: " + totalTimeElapsed); }, 200);
 })
